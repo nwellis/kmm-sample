3cf7624f99c2f8bce3060e66bf770f75ff666b5f
Nick Ellis <nicholas.w.ellis@gmail.com>
2021-01-02 11:31:47 -0600

    fix(iosMain): use built in NSLogLogger from Kermit

--- shared/src/iosMain/kotlin/me/nickellis/kmmsample/shared/Platform.kt
+++ shared/src/iosMain/kotlin/me/nickellis/kmmsample/shared/Platform.kt
@@ -2,23 +2,11 @@ package me.nickellis.kmmsample.shared
 
 
 import co.touchlab.kermit.Logger
-import co.touchlab.kermit.PlatformThrowableStringProvider
-import co.touchlab.kermit.Severity
-import platform.Foundation.NSLog
+import co.touchlab.kermit.NSLogLogger
 import platform.UIKit.UIDevice
 
 actual class Platform actual constructor() {
     actual val platform: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 
-    //actual val logger: Logger = NSLogLogger()
-    actual val logger: Logger = object : Logger() {
-        // TODO: Why cant I import NSLogLogger?
-        override fun log(severity: Severity, message: String, tag: String, throwable: Throwable?) {
-            NSLog("%s: (%s) %s", severity.name, tag, message)
-            throwable?.let {
-                val string = PlatformThrowableStringProvider().getThrowableString(it)
-                NSLog("%s", string)
-            }
-        }
-    }
+    actual val logger: Logger = NSLogLogger()
 }
